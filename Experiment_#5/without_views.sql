0114e2a001e2ddd14702dfc4f437d77aa2af57d6
Md. Rafiz Uddin <77089688+rafiz001@users.noreply.github.com>
2024-10-05 19:46:39 +0600

    Update without_views.sql

--- Experiment_#5/without_views.sql
+++ Experiment_#5/without_views.sql
@@ -46,14 +46,40 @@ insert into customer (customer_id,cust_name,city,grade,salesman_id) values
 
 insert into orders (ord_no, purch_amt,ord_date,customer_id,salesman_id) values 
 (70001, 150.5, '2012-10-05', 3005, 5002),
-(, , , , )
-(, , , , )
-(, , , , )
-(, , , , )
-(, , , , )
-(, , , , )
-(, , , , )
-(, , , , )
-(, , , , )
-(, , , , )
-(, , , , )
+(70009, 270.65, '2012-09-10', 3001, 5005),
+(70002, 65.26, '2012-10-05', 3002, 5001),
+(70004, 110.5, '2012-08-17', 3009, 5003),
+(70007, 948.5, '2012-09-10', 3005, 5002),
+(70005, 2400.6, '2012-07-27', 3007, 5001),
+(70008, 5760, '2012-09-10', 3002, 5001),
+(70010, 1983.43, '2012-10-10', 3004, 5006),
+(70003, 2480.4, '2012-10-10', 3009, 5003),
+(70012, 250.45, '2012-06-27', 3008, 5002),
+(70011, 75.29, '2012-08-17', 3003, 5007),
+(70013, 3045.6, '2012-04-25', 3002, 5001);
+
+
+-- 1. Write a query to create a view for those salesmen belongs to the city New York.
+select * from salesman where city = 'New York';
+
+-- 2. Write a query to create a view for all salesmen with columns salesman_id, name and city.
+select salesman_id, name, city from salesman;
+
+-- 3. Write a query to find the salesmen of the city New York who achieved the commission more than 13%.
+select * from salesman where city = 'New York' and commission > 0.13;
+
+-- 4. Write a query to create a view to getting a count of how many customers we have at each level of a grade.
+select count(*) as count, grade from customer group by grade;
+
+-- 5. Write a query to create a view to keeping track the number of customers ordering, number of salesmen attached, average amount of orders and the total amount of orders in a day.
+select count(customer_id) as Customer, count(salesman_id) as Salesmen, avg(purch_amt) as Average_Purchase, sum(purch_amt) as Total_Purchase from orders where ord_date = '2012-10-10';
+
+-- 6. Write a query to create a view that shows for each order the salesman and customer by name.
+select orders.*, salesman.name as salesman_name, customer.cust_name as customer_name from orders 
+inner join customer on orders.customer_id=customer.customer_id
+inner join salesman on orders.salesman_id=salesman.salesman_id;
+
+-- 7. Write a query to create a view that finds the salesman who has the customer with the highest order of a day.
+select name from salesman where salesman_id = 
+(select salesman_id from orders where )
+where 
